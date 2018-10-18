349a37f5f6cf7b42c049b6cdaf21c3893d5f0506
Bartek Pichalski <bartek.pichalski@mohemian.com>
2018-10-18 14:13:32 +0200

    Added Extension file and first helper metod

---
+++ ARPortalDD/Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  Extensions.swift
+//  ARPortalDD
+//
+//  Created by Bartek Pichalski on 18/10/2018.
+//  Copyright © 2018 com.pichalski.bartek. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+import ARKit
+
+var width: CGFloat = 0.2
+var height: CGFloat = 1
+var lenght: CGFloat = 1
+
+func createBox(isDoor: Bool) -> SCNNode {
+    let node = SCNNode()
+
+    // Add first box
+    let firstBox = SCNBox(width: width, height: height, length: lenght, chamferRadius: 0)
+    let firstBoxNode = SCNNode(geometry: firstBox)
+    firstBoxNode.renderingOrder = 200
+    node.addChildNode(firstBoxNode)
+
+    //Add masked box
+    let maskedBox = SCNBox(width: width, height: height, length: lenght, chamferRadius: 0)
+    maskedBox.firstMaterial?.diffuse.contents = UIColor.white
+    maskedBox.firstMaterial?.transparency = 0.00001
+
+    let maskedBoxNode = SCNNode(geometry: maskedBox)
+    maskedBoxNode.renderingOrder = 100
+    maskedBoxNode.position = SCNVector3.init(width, 0, 0)
+
+    node.addChildNode(maskedBoxNode)
+
+    return node
+}
